07d74af1325205bec1264378641e80d7b1d66deb
alejandro gallo <alejandroamsg@hotmail.com>
2016-01-17 22:38:31 +0100

    Initial commit

---
+++ js/fortex.js
@@ -0,0 +1,24 @@
+MathJax.Hub.Config({
+  tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
+});
+
+angular.module('fortex', [])
+.controller("Widget", ["$scope", function ($scope) {
+  $scope.formula="\\int";
+}])
+.directive("mathjaxBind", function() {
+  return {
+    restrict: "A",
+    controller: ["$scope", "$element", "$attrs",
+    function($scope, $element, $attrs) {
+      $scope.$watch($attrs.mathjaxBind, function(texExpression) {
+        var texScript = angular.element("<script type='math/tex'>")
+          .html(texExpression ? texExpression :  "");
+        $element.html("");
+        $element.append(texScript);
+        MathJax.Hub.Queue(["Reprocess", MathJax.Hub, $element[0]]);
+      });
+    }]
+  };
+});
+
